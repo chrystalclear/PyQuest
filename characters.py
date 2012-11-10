d01d272b4fb70bd265ae9b245986a9814106729d
Jonathan Ting <jonathan.ting@berkeley.edu>
2012-11-09 22:08:58 -0800

    added initializations for enemies, encounters, and location

--- characters.py
+++ characters.py
@@ -1,29 +1,68 @@
+import random
+
 #Starts the game, gets the Player's name
 def start():
-	init_characters()
-	#init_items()
+	init_enemies()
+	init_encounters()
+	init_items()
+	init_locations()
 
 	print('--Beginning Story--')
 	char_name = input('What is your name? ')
-	player = Player(char_name)
-	print('Well hello, ' + player.name + '.')
+	player = Player(char_name, location("Dorm"))
+	print('Well hello, ' + player.name + '.\n')
 	main(player)
 
-characters, items = [], []
+enemies, encounters, items, locations = [], [], [], []
 
-#Initialize all characters
-def init_characters():
+#Initialization
+def init_enemies():
 	boss0 = Enemy("Boss0", "MCB")
-	characters.extend([boss0])
+	enemies.extend([boss0])
+
+def init_encounters():
+	friend0 = Encounters("Friend A")
 
 def init_items():
 	item0 = Item("Fruit", "HP", "25")
-	item1 = Item("5-hour energy", "Energy")
+	item1 = Item("5-hour energy", "Energy", "25")
 	items.extend([item0, item1])
+
+def init_locations():
+	campanile = Location("Campanile")
+	dorm = Location("Dorm")
+	outside = Location("Outside")
+	floor1 = Soda("Soda", 1)
+	floor2 = Soda("Soda", 2)
+	floor3 = Soda("Soda", 3)
+	floor4 = Soda("Soda", 4)
+	floor5 = Soda("Soda", 5)
+	floor6 = Soda("Soda", 6)
+	locations.extend([campanile, dorm, outside, floor1, floor2, floor3, floor4, floor5, floor6])
 
 #Main Function where everything happens
 def main(player):
-	print('')
+	if player.location.name == "Campanile":
+		print('You are at the Campanile')
+	elif player.location.name == "Dorm":
+		print('You are inside your dorm')
+	elif player.location.name == "Outside":
+		print('You are outside')
+	elif player.location.name == "Soda":
+		print('You are inside Soda, floor ' + player.location.floor)
+
+def location(name, floor = None):
+	for location in locations:
+		if location.name == name and floor == None:
+			return location
+		elif floor:
+			if location.name == name and location.floor == floor:
+				return location
+	return None
+
+def encounter():
+	if random.random() < 0.05:
+		return True
 
 #Classes for Character, Major, Item, Player, Enemy
 class Character(object):
@@ -47,13 +86,27 @@ class Item(object):
 		self.role = role
 		self.increase = increase
 
-class Player(Character):
+class Location(object):
 	def __init__(self, name):
+		self.name = name
+
+class Soda(Location):
+	def __init__(self, name, floor):
+		Location.__init__(self, name)
+		self.floor = floor
+
+class Player(Character, Location):
+	def __init__(self, name, location = None):
 		Character.__init__(self, name)
+		self.location = location
 
 class Enemy(Character, Major):
 	def __init__(self, name, major):
 		Character.__init__(self, name)
 		Major.__init__(self, major)
+
+class Encounters(Character):
+	def __init__(self, name):
+		Character.__init__(self, name)
 
 start()
