1d62dedd6823b7dd42d204d5595df1b2f01c7f8c
Dylan Chun <d.chunwong@gmail.com>
2012-11-09 22:32:32 -0800

    added 4 classes

--- majors.py
+++ majors.py
@@ -2,6 +2,10 @@ class Major(object):
 	def __init__(self, name):
 		self.moves={}
 		self.multipliers={'History': 1, 'MCB': 1, 'Haas': 1, 'EECS': 1}
+
+	def get(self, action):
+		if action in self.moves:
+			return self.moves[action]
 		 
 class EECS(major):
 	def __init__(self, name):
@@ -9,8 +13,21 @@ class EECS(major):
 		self.multipliers=major.multipliers.copy()
 		self.multipliers.update(EECS=.5, History=2)
 	
-	def get(self, action):
-		if action in self.moves:
-			return self.moves[action]
 
-	def nogirlsallowed
+class History(major):
+	def __init__(self, name):
+		self.moves={}
+		self.multipliers=major.multipliers.copy()
+		self.multipliers.update(MCB=2, EECS=.5)
+
+class MCB(major):
+	def __init__(self, name):
+		self.moves={}
+		self.multipliers=major.multipliers.copy()
+		self.multipliers.update(History=.5, Haas=2)
+
+class Haas(major):
+	def __init__(self, name):
+		self.moves={}
+		self.multipliers=major.multipliers.copy()
+		self.multipliers.update(MCB=.5, EECS=2)
