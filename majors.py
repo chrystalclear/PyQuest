e4c43d4d13120abf0d4ebceb3010c52b32b763c3
Dylan Chun <d.chunwong@gmail.com>
2012-11-09 21:53:34 -0800

    fuckin shit up

--- majors.py
+++ majors.py
@@ -1,6 +1,16 @@
 class major(object):
 	def __init__(self, name):
 		self.moves={}
-		self.multipliers={'History': 1, 'Math': 1, 'Haas': 1, 'EECS': 1}
+		self.multipliers={'History': 1, 'MCB': 1, 'Haas': 1, 'EECS': 1}
 		 
+class EECS(major):
+	def __init__(self, name):
+		self.moves={}
+		self.multipliers=major.multipliers.copy()
+		self.multipliers.update(EECS=.5, History=2)
+	
+	def get(self, action):
+		if action in self.moves:
+			return self.moves[action]
 
+	def nogirlsallowed
