d1f075b33ba092fc3357c7e41f4ec2d3eed69856
Stephanie Djidjev <sdjidjev@gmail.com>
2012-11-09 22:35:08 -0800

    infiniterecursionattack added

--- majors.py
+++ majors.py
@@ -2,6 +2,11 @@ class Major(object):
 	def __init__(self, name):
 		self.moves={}
 		self.multipliers={'History': 1, 'MCB': 1, 'Haas': 1, 'EECS': 1}
+
+	def get(self, action):
+		if action in self.moves:
+			return self.moves[action]
+
 		 
 class EECS(major):
 	def __init__(self, name):
@@ -9,8 +14,13 @@ class EECS(major):
 		self.multipliers=major.multipliers.copy()
 		self.multipliers.update(EECS=.5, History=2)
 	
-	def get(self, action):
-		if action in self.moves:
-			return self.moves[action]
 
-	def nogirlsallowed
+	@property
+	def InfiniteRecursion:
+		print("Stack overflow. Opponent "+str(eval(repr(enemy.major)))+" has crashed.")
+		enemy.stats['health'] -= 1 
+		print("Enemy stats: "+str(s) for s in enemy.stats)
+
+
+
+
